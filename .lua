9aec0c93b08a3592ee8e2ca18ec96f51ab1f695f
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-04-28 12:50:43 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,14 +1,53 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
 local wndw = lib:Window("VIP Turtle Hub V4")
 local T1 = wndw:Tab("Main")
+local T2 = wndw:Tab("Character")
+local T3 = wndw:Tab("Enemies")
+
 local workspace = game:GetService("Workspace")
 
 local var = {
   swing = false,
   id = 577401600,
   parry = false,
-  ability = {false,false}
+  ability = {false,false},
+  self = game.Players.LocalPlayer,
+  speed = 20,
+  jump = 50,
+  ls = false,
+  lj = false,
+  cd = false
 }
+--client.Character.Humanoid.WalkSpeed = 
+--client.Character.Humanoid.JumpPower = 
+
+T3:Label(lib:ColorFonts("You dont have access to see this feature.","Red"))
+
+T2:Slider("Custom speed",0,100,20,function(value)
+    var.speed = value
+end)
+
+T2:Slider("Custom jump",0,500,50,function(value)
+    var.jump = value
+end)
+
+T2:Toggle("Speed bypass",false,function(value)
+    var.ls = value
+    while wait() do
+      if var.ls == false then break end
+      var.self.Character.Humanoid.WalkSpeed = var.speed
+    end
+end)
+
+T2:Toggle("Jump bypass",false,function(value)
+    var.lj = value
+    while wait() do
+      if var.lj == false then break end
+      var.self.Character.Humanoid.JumpPower = var.jump
+    end
+end)
+
+T1:Label("Attack first to use this.")
 
 T1:Toggle("Auto swing enemy",false,function(value)
     var.swing = value
